e45df0c5af115f344617bb3c3ba03bbdc7c184a6
Fatima Zohra Jaber <jaber.fatiima.zohra@gmail.com>
2026-04-21 09:30:51 +0100

    Add cabin count and revalidate every 24 hours

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,17 @@
+import { getCabins } from "@/lib/data.service";
 import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
+export const revalidate = 86400; // Revalidate the page every 24 hours (86400 seconds)
+
 export const metadata: Metadata = {
   title: "About",
 };
+  
+export default async function About() {
+  const cabins = await getCabins();
 
-export default function About() {
   return (
     <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
       <div className="col-span-3">
@@ -23,10 +28,10 @@ export default function About() {
             and enjoying simple pleasures with family.
           </p>
           <p>
-            Our 8 luxury cabins provide a cozy base, but the real freedom and
-            peace you&apos;ll find in the surrounding mountains. Wander through
-            lush forests, breathe in the fresh air, and watch the stars twinkle
-            above from the warmth of a campfire or your hot tub.
+            Our {cabins.length} luxury cabins provide a cozy base, but the real
+            freedom and peace you&apos;ll find in the surrounding mountains.
+            Wander through lush forests, breathe in the fresh air, and watch the
+            stars twinkle above from the warmth of a campfire or your hot tub.
           </p>
           <p>
             This is where memorable moments are made, surrounded by
